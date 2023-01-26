772a7c7ee0d0d066538dbf1805b0d80820066cef
Egor <hazinega@gmail.com>
2023-01-26 16:57:33 +0300

    add else if

--- script.js
+++ script.js
@@ -303,4 +303,19 @@ function testLogicalOr(val) {
     }
 }
 
-testLogicalOr(15);
+// testLogicalOr(15);
+
+function testElseIf(val) {
+    if (val > 10) {
+        return "Greater than 10";
+    } else
+
+    if (val < 5) {
+        return "Smaller than 5";
+    } else {
+
+        return "Between 5 and 10";
+    }
+}
+
+console.log(testElseIf(7));
