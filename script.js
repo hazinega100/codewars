f703405d8b7636ceb4c23934692cd0150c97c2a2
Egor <hazinega@gmail.com>
2023-01-28 12:51:47 +0300

    add function likes

--- script.js
+++ script.js
@@ -392,4 +392,43 @@ function sortArray(array) {
         .map(x => x % 2 ? odds.shift() : x);
 }
 
-console.log(sortArray([5, 3, 2, 8, 1, 4]));
+// console.log(sortArray([5, 3, 2, 8, 1, 4]));
+
+function solution(str) {
+    return str.split('').reverse().join('');
+}
+
+// console.log(solution('world'));
+
+function likes(names) {
+    if (names.length === 0) {
+        return "no one likes this";
+    } else if (names.length === 1) {
+        return `${names[0]} likes this`;
+    } else if (names.length === 2) {
+        return `${names[0]} and ${names[1]} like this`;
+    } else if (names.length === 3) {
+        return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+    } else {
+        return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
+    }
+
+    //     names = names || [];
+    //   switch(names.length){
+    //     case 0: return 'no one likes this'; break;
+    //     case 1: return names[0] + ' likes this'; break;
+    //     case 2: return names[0] + ' and ' + names[1] + ' like this'; break;
+    //     case 3: return names[0] + ', ' + names[1] + ' and ' + names[2] + ' like this'; break;
+    //     default: return names[0] + ', ' + names[1] + ' and ' + (names.length - 2) + ' others like this';
+    //   }
+
+    // return {
+    //     0: 'no one likes this',
+    //     1: `${names[0]} likes this`, 
+    //     2: `${names[0]} and ${names[1]} like this`, 
+    //     3: `${names[0]}, ${names[1]} and ${names[2]} like this`, 
+    //     4: `${names[0]}, ${names[1]} and ${names.length - 2} others like this`, 
+    //   }[Math.min(4, names.length)]
+}
+
+// console.log(likes(['Peter']));
